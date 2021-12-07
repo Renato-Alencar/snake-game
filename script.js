0c07ed84c1ebe795b31a73d693eb541f9c30c568
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-12-07 16:36:48 -0300

    Cria a cobrinha do jogo.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let canvas = document.getElementById("snake-game");
 let context = canvas.getContext("2d");
 let box = 32;
-// let snake = [];
-// snake[0] = {
-//   x: 8 * box,
-//   y: 8 * box,
-// };
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
 
 function createBG() {
   context.fillStyle = "burlywood";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-// function createSnake() {
-//   snake.forEach((position) => {
-//     context.fillStyle = "green";
-//     context.fillRect(position.x, position.y, box, box);
-//   });
-// }
+function createSnake() {
+  snake.forEach((position) => {
+    context.fillStyle = "green";
+    context.fillRect(position.x, position.y, box, box);
+  });
+}
 
 createBG();
-// createSnake();
+createSnake();
